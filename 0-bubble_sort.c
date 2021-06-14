ef6e8870fa28c6f519f8bf32c35e5b8fbc7f09a3
YerardinPerlaza <yerardinpm@gmail.com>
2021-06-14 15:05:16 +0000

    fix betty of bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,29 +7,27 @@
 */
 void bubble_sort(int *array, size_t size)
 {
-    int tmp = 0;
-    size_t counter = 0, i = 0;
+	int tmp = 0;
+	size_t counter = 0, i = 0;
 
-    if (array == NULL || size < 2)
+	if (array == NULL || size < 2)
 		return;
 
-    for (counter = 0; size - 1 != counter;)
-    {
-        counter = 0;
+	for (counter = 0; size - 1 != counter;)
+	{
+		counter = 0;
 
-        for (i = 0; i + 1 != size; i++)
-        {
-            if (array[i + 1] < array[i])
-            {
-                tmp = array[i];
-                array[i] = array[i + 1];
-                array[i + 1] = tmp;
-                print_array(array, size);
-            }
-            else
-            {
-                counter++;
-            }
-        }
-    }
+		for (i = 0; i + 1 != size; i++)
+		{
+			if (array[i + 1] < array[i])
+			{
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
+				print_array(array, size);
+			}
+			else
+				counter++;
+		}
+	}
 }
